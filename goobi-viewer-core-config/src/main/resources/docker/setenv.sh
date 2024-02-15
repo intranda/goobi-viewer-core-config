b34d626290774856f14e8172730d926bd458b135
Matthias Geerdsen <matthias.geerdsen@intranda.com>
2024-02-15 14:41:01 +0100

    fix(docker): remove deprecated Java option in order to use Java 17 (refs #25773)

--- goobi-viewer-core-config/src/main/resources/docker/setenv.sh
+++ goobi-viewer-core-config/src/main/resources/docker/setenv.sh
@@ -3,5 +3,4 @@
 export JAVA_OPTS="${JAVA_OPTS} -XX:+UseG1GC"
 export JAVA_OPTS="${JAVA_OPTS} -XX:+ParallelRefProcEnabled"
 export JAVA_OPTS="${JAVA_OPTS} -XX:+DisableExplicitGC"
-export JAVA_OPTS="${JAVA_OPTS} -XX:+CMSClassUnloadingEnabled"
 export JAVA_OPTS="${JAVA_OPTS} -Dfile.encoding='utf-8'"
