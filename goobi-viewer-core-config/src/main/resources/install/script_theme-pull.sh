4f8b352e4cce2ef2130f42be6dc0b3b293808d40
Florian Alpers <florian.apers@intranda.com>
2024-03-08 12:10:41 +0100

    Updated bash scripts and added config for bash script calls

--- goobi-viewer-core-config/src/main/resources/install/script_theme-pull.sh
+++ goobi-viewer-core-config/src/main/resources/install/script_theme-pull.sh
@@ -5,17 +5,16 @@
 #
 # The script expects one parameter: the path to the git repository of the theme
 #
-# The script should return one of three return values:
+# The script should return one of two return values:
 #
-# 0 - if the repository was successfully pulled
-# 1 - if the pull command returned "Already up to date", i.e. no changes were detected to pull
-# 2 - if an error occured. In this case an error description should be passed to the error output stream
+# 0 - if the repository was successfully pulled or if no changes where detected. In the latter case, the script should write "Git repository is already up to date." to the output stream
+# 1 - if an error occured. In this case an error description should be passed to the error output stream
 #
 
 # Check if the repository path parameter is provided
 if [ -z "$1" ]; then
     echo "Error: Repository path not provided." >&2
-    exit 2
+    exit 1
 fi
 
 repository_path="$1"
@@ -23,7 +22,7 @@ repository_path="$1"
 # Check if the repository path is non-empty and points to an existing directory
 if [ -z "$repository_path" ] || [ ! -d "$repository_path" ]; then
     echo "Error: Invalid repository path or directory does not exist." >&2
-    exit 2
+    exit 1
 fi
 
 pull_output=$(git -C "$repository_path" pull);
@@ -32,7 +31,7 @@ pull_output=$(git -C "$repository_path" pull);
 if [[ $pull_output == *"Already up to date."* ]]; then
     # Return 1 to indicate no changes
     echo "Git repository is already up to date."
-    exit 1
+    exit 0
 elif [ $? -eq 0 ]; then
     # Exit status 0 indicates success (changes were pulled)
     echo "Git pull successful: Changes pulled"
@@ -41,6 +40,6 @@ else
     # exit status 2 indicates an error (e.g., conflicts or network issues)
     echo "Error: Git pull failed."
     echo "$pull_output" >&2
-    exit 2
+    exit 1
 fi
 
